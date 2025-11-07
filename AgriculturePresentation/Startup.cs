8b97cf41be1ccc1a4d7177a0b97874674e735a80
Münevver Orman <munevverorman1@gmail.com>
2025-11-07 15:52:11 +0300

    Takım varlığı için CRUD (Oluşturma, Okuma, Güncelleme, Silme) operasyonları, FluentValidation kullanılarak doğrulama ile eklendi. Servis görünümleri, kart tabanlı bir tasarıma güncellendi. FluentValidation.AspNetCore paketi hem AgriculturePresentation hem de BusinessLayer projelerine dahil edildi. Takım üyeleri ekleme ve düzenleme işlemleri için yeni görünümler (view'ler) eklendi

--- AgriculturePresentation/Startup.cs
+++ AgriculturePresentation/Startup.cs
@@ -30,6 +30,9 @@ namespace AgriculturePresentation
         {
             services.AddScoped<IServiceService,ServiceManager>();
             services.AddScoped<IServiceDal,EfServiceDal>();
+            services.AddScoped<ITeamService, TeamManager>();
+            services.AddScoped<ITeamDal, EfTeamDal>();
+
             services.AddDbContext<AgricultureContext>();
             services.AddControllersWithViews();
         }
